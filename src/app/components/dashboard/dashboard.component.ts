b887c09c98745c547a40ed9950463a1a2cc95280
Mario Muñoz <mario.munoz@fibramax.ec>
2024-12-19 15:36:15 -0500

    Estilos side nav-bar

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true, 
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss'],
+  imports: [CommonModule] 
 })
 export class DashboardComponent {
-
+  // Datos de prueba para la tabla
+  data = [
+    { id: 1, name: 'John Doe', email: 'john.doe@example.com', role: 'Admin' },
+    { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com', role: 'User' },
+    { id: 3, name: 'Sam Brown', email: 'sam.brown@example.com', role: 'Manager' },
+    { id: 4, name: 'Lisa White', email: 'lisa.white@example.com', role: 'User' }
+  ];
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,7 +7,7 @@ export class AuthService {
   private tokenKey = 'authToken';
 
   login(credentials: { email: string; password: string }) {
-    return fetch('http://127.0.0.1:8000/api/get-user/1UliJqH6oNuZEk6l72r7alxHe5QOyYGS6ZzS8NtyfYP4/materiales-fibramax-65bb0c225f90.json', {
+    return fetch('https://api.tvmax.ec/api/get-user/1UliJqH6oNuZEk6l72r7alxHe5QOyYGS6ZzS8NtyfYP4/materiales-fibramax-65bb0c225f90.json', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(credentials),
