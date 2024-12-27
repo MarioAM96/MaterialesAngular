7ca79e5a5b9b788ffbd90242206c220d553f78fc
Mario Muñoz <mario.munoz@fibramax.ec>
2024-12-27 13:54:31 -0500

    mejora estilos sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -2,19 +2,45 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SidebarService } from '../../services/sidebar.service';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faHome,
+  faClipboardList,
+  faTruck,
+  faCog,
+  faTasks,
+  faUsers,
+  faCubes,
+  faUser
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
 })
 export class SidebarComponent implements OnInit {
   userName: string = '';
   userRol: string = '';
   isCollapsed: boolean = false;
 
-  constructor(private sidebarService: SidebarService) {}
+  constructor(
+    private sidebarService: SidebarService,
+    private iconLibrary: FaIconLibrary
+  ) {
+    this.iconLibrary.addIcons(
+      faHome,
+      faClipboardList,
+      faTruck,
+      faCog,
+      faTasks,
+      faUsers,
+      faCubes,
+      faUser
+    );
+  }
 
   ngOnInit() {
     const storedUserName = sessionStorage.getItem('userName');
